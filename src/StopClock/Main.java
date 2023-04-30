6ffe79917c056aba1a55641b4c9cb9daa94fba5b
Naushad Ahmad <naushadkhandmg@gmail.com>
2023-04-30 10:00:01 +0530

    first project

---
+++ src/StopClock/Main.java
@@ -0,0 +1,10 @@
+package StopClock;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 StopWatch stopwatch = new StopWatch();
+	}
+
+}
---
+++ src/Timer/Main.java
@@ -0,0 +1,10 @@
+package Timer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   new TimerWatch();
+	}
+
+}
